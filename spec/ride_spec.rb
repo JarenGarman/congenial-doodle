8bdda576e9cc2a12deb0cd88dd0d3fe8fb6c6067
Jaren Garman <jarengarman@protonmail.com>
2025-01-21 11:25:23 -0500

    test: Add tests for initialize method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -9,5 +9,25 @@ RSpec.describe Ride do
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
+
+    it 'has a name' do
+      expect(first_ride.name).to eq('Carousel')
+    end
+
+    it 'has a minimum height' do
+      expect(first_ride.min_height).to eq(24)
+    end
+
+    it 'has an admission fee' do
+      expect(first_ride.admission_fee).to eq(1)
+    end
+
+    it 'has an excitement level' do
+      expect(first_ride.excitement).to eq(:gentle)
+    end
+
+    it 'has no revenue' do
+      expect(first_ride.total_revenue).to eq(0)
+    end
   end
 end
