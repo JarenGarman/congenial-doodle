a6dd4a74d5af5c740d14652f27ba5bd5abd67d80
Jaren Garman <jarengarman@protonmail.com>
2025-01-21 11:11:56 -0500

    test: Add tests to round off initialize method

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -7,5 +7,21 @@ RSpec.describe Visitor do
 
   describe '#initialize' do
     it { is_expected.to be_instance_of described_class }
+
+    it 'has a name' do
+      expect(first_visitor.name).to eq('Bruce')
+    end
+
+    it 'has a height' do
+      expect(first_visitor.height).to eq(54)
+    end
+
+    it 'has spending money' do
+      expect(first_visitor.spending_money).to eq(10)
+    end
+
+    it 'has no preferences' do
+      expect(first_visitor.preferences).to eq([])
+    end
   end
 end
