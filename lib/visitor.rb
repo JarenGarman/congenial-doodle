c2df8c1e0044901d9bd590c26098114a4dca72a6
Jaren Garman <jarengarman@protonmail.com>
2025-01-21 11:13:13 -0500

    feat: Add name, height, spending_money, and preferences attributes

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -2,6 +2,12 @@
 
 # Create a visitor for the carnival
 class Visitor
+  attr_reader :name, :height, :spending_money, :preferences
+
   def initialize(name, height, spending_money)
+    @name = name
+    @height = height
+    @spending_money = spending_money.delete_prefix('$').to_i
+    @preferences = []
   end
 end
