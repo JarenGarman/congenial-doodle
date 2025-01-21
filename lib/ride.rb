fb747be80ce691dcb04f4c5848c84e3f149817c1
Jaren Garman <jarengarman@protonmail.com>
2025-01-21 11:26:45 -0500

    feat: Add attributes for name, min_height, admission_fee, excitement, and total_revenue

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,6 +2,13 @@
 
 # Create a ride for the carnival
 class Ride
+  attr_reader :name, :min_height, :admission_fee, :excitement, :total_revenue
+
   def initialize(ride_params)
+    @name = ride_params[:name]
+    @min_height = ride_params[:min_height]
+    @admission_fee = ride_params[:admission_fee]
+    @excitement = ride_params[:excitement]
+    @total_revenue = 0
   end
 end
